5ed2e561394f9f087e0b9de2efe69f0a06d33f79
DNyaika <davie2086@gmail.com>
2015-01-10 17:46:55 +0100

    Great we can now post on mateos time line!!

--- src/it/unitn/introsde/mbeans/FaceBookMBean.java
+++ src/it/unitn/introsde/mbeans/FaceBookMBean.java
@@ -1,30 +1,44 @@
 package it.unitn.introsde.mbeans;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+import com.restfb.types.Page;
+import com.restfb.types.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-import javax.annotation.PostConstruct;
-
-
 import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import java.io.Serializable;
 import java.util.Map;
 
 /**
  * Created by davie on 1/10/2015.
  */
-@Named
+@ManagedBean(name = "faceBookMBean", eager = true)
 @SessionScoped
 public class FaceBookMBean implements Serializable {
 
     private static final Logger logger = LogManager.getLogger();
-    public String submit() {
+
+    public void submit() {
+        System.out.println("reaching here>>>");
         Map<String, String> requestParameters = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
         String fbaccesstoken = requestParameters.get("fbform:fbaccesstoken");
-        logger.info("***** fbaccesstoken: " + fbaccesstoken);
-        return fbaccesstoken;
+
+        //getting details mateo has permitted us to access
+        FacebookClient facebookClient = new DefaultFacebookClient(fbaccesstoken);
+        User user = facebookClient.fetchObject("me", User.class);
+        Page page = facebookClient.fetchObject("cocacola", Page.class);
+
+        System.out.println("User name: " + user.getName());
+        System.out.println("Page likes: " + page.getLikes());
+
+        //posting on mateos timeline
+        FacebookType publishMessageResponse = facebookClient.publish("me/feed", FacebookType.class, Parameter.with("message", "RestFB test"));
+        System.out.println("Published message ID: " + publishMessageResponse.getId());
     }
 }
