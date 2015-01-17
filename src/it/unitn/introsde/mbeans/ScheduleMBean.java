4e974f80b9fbd0d07cafb03600299afe26241803
DNyaika <davie2086@gmail.com>
2015-01-17 12:08:40 +0100

    creating and adding event to google calendar

--- src/it/unitn/introsde/mbeans/ScheduleMBean.java
+++ src/it/unitn/introsde/mbeans/ScheduleMBean.java
@@ -1,16 +1,23 @@
 package it.unitn.introsde.mbeans;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Calendar;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
 import com.google.api.services.calendar.model.EventDateTime;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import java.io.File;
 import java.io.Serializable;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * Created by davie on 1/10/2015.
@@ -18,13 +25,62 @@ import java.util.Map;
 @ManagedBean(name = "scheduleMBean", eager = true)
 @SessionScoped
 public class ScheduleMBean implements Serializable {
-    String googleaccesstoken;
+
 
     public void submit() throws Exception {
+
         Map<String, String> requestParameters = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-       googleaccesstoken = requestParameters.get("googleform:googleaccesstoken");
-        System.out.println("googleaccesstoken>>>>>>  "+googleaccesstoken);
-       //TODO
-        //create calendar and and add events
+        String googleaccesstoken = requestParameters.get("googleform:googleaccesstoken");
+        //System.out.println("googleaccesstoken>>>>>>  " + googleaccesstoken);
+        createCalendar(googleaccesstoken);
+       createEvent(googleaccesstoken);
+    }
+
+    public void createEvent(String accessToken) throws Exception {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
+        // Initialize Calendar service with valid OAuth credentials
+        com.google.api.services.calendar.Calendar service = new com.google.api.services.calendar.Calendar.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("LifestyleCoach").build();
+
+        // Create and initialize a new event
+        Event event = new Event();
+        event.setSummary("walk 300km");
+        event.setLocation("queens park");
+
+        ArrayList<EventAttendee> attendees = new ArrayList<EventAttendee>();
+        attendees.add(new EventAttendee().setEmail("matteo.matteovich@gmail.com"));
+// ...
+        event.setAttendees(attendees);
+
+        Date startDate = new Date();
+        Date endDate = new Date(startDate.getTime() + 3600000);
+        DateTime start = new DateTime(startDate, TimeZone.getTimeZone("UTC"));
+        event.setStart(new EventDateTime().setDateTime(start));
+        DateTime end = new DateTime(endDate, TimeZone.getTimeZone("UTC"));
+        event.setEnd(new EventDateTime().setDateTime(end));
+
+// Insert the new event
+        Event createdEvent = service.events().insert("primary", event).execute();
+
+        System.out.println(createdEvent.getId());
+    }
+
+    public void createCalendar(String accessToken) throws Exception {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
+
+        com.google.api.services.calendar.Calendar service = new com.google.api.services.calendar.Calendar.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("LifestyleCoach").build();
+
+        // Create a new calendar
+        com.google.api.services.calendar.model.Calendar calendar = new Calendar();
+        calendar.setSummary("MateoCalendar");
+
+        // Insert the new calendar
+        com.google.api.services.calendar.model.Calendar createdCalendar = service.calendars().insert(calendar).execute();
+        System.out.println(createdCalendar.getId());
     }
 }
